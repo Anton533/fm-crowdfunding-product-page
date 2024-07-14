fa5c085b2e818ddc33a469febd4bb02acb478c6f
Anton <anton.omelianenko533@gmail.com>
2024-07-14 20:14:08 +0300

    add feedback modal

--- src/js/script.js
+++ src/js/script.js
@@ -10,3 +10,22 @@ bookmarkEl.addEventListener('click', () => {
     document.querySelector('.bookmark__text').innerText = 'Bookmarked';
   }
 });
+
+const selectReward = document.getElementById('select-reward-1');
+
+selectReward.addEventListener('click', () => {
+  const modal = document.getElementById('feedback-modal');
+  if (!modal) return;
+  const body = document.querySelector('body');
+  // body.classList.add('modal-open');
+  modal.classList.add('is-open');
+});
+
+const gotItBtn = document.getElementById('got-it-btn');
+
+gotItBtn.addEventListener('click', () => {
+  const modal = document.getElementById('feedback-modal');
+  const body = document.querySelector('body');
+  // body.classList.remove('modal-open');
+  modal.classList.remove('is-open');
+});
